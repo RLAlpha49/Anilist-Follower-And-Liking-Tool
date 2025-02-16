6db603734c23ae863e2a13e4f9045da92a666a81
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2025-02-16 05:13:42 -0600

    Create javascript webpack wrapper files

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,21 +1,21 @@
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
 
-import { rules } from './webpack.rules.ts';
-import { plugins } from './webpack.plugins.ts';
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+	test: /\.css$/,
+	use: [{ loader: "style-loader" }, { loader: "css-loader" }],
 });
 
 const rendererConfig: Configuration = {
-  module: {
-    rules,
-  },
-  plugins,
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
-  },
+	module: {
+		rules,
+	},
+	plugins,
+	resolve: {
+		extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
+	},
 };
 
 export default rendererConfig;
