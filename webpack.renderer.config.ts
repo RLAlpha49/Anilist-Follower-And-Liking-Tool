5312befdd39a4e3cd85a9bc685335061adc60648
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2025-02-16 04:46:00 -0600

    Initialize new electron app

---
+++ webpack.renderer.config.ts
@@ -0,0 +1,21 @@
+import type { Configuration } from 'webpack';
+
+import { rules } from './webpack.rules.ts';
+import { plugins } from './webpack.plugins.ts';
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+const rendererConfig: Configuration = {
+  module: {
+    rules,
+  },
+  plugins,
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+  },
+};
+
+export default rendererConfig;
